10203c9c9d5cd91dd8bd8cdca6b64671cf527d3f
william-le1004 <kienle4456@gmail.com>
2025-03-28 23:00:02 +0700

    [Send Mail] - Create Template, Add Log

--- Application/Events/UserCreated.cs
+++ Application/Events/UserCreated.cs
@@ -2,6 +2,7 @@ namespace Application.Events;
 
 public record UserCreated
 {
+    public string Name { get; init; }
     public string Email { get; init; }
     public string UserName { get; init; }
     public string Password { get; init; }
